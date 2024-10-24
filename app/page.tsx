fdff97c86902df8916e481cd48ec20f1baa5261a
Sofus <sofus_kildegaard@me.com>
2024-10-24 21:33:05 +0200

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+    return(
+      <section className="py-24">
+        <div className="container">
+          <h1>Hello World</h1>
+        </div>
+      </section>
+    )
+}
